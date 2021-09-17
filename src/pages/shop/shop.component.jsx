f59de58dec2549c92b559f7debaed6aa01542a55
mjstring <mk.string@pm.me>
2021-09-17 16:02:06 -0500

    preparing routing for shop categories

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
 
-import React from 'react';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
-import SHOP_DATA from './shop.data.js';
+import CollectionPage from '../collection/collection.component'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({match}) => (
+  <div className="shop-page">
+    <Route exact path ={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+  </div>
+);
 
 export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import { persistReducer } from "redux-persist";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
@@ -12,7 +14,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
