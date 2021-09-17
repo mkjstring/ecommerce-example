c8b3f623e8509850bc0553cdbae1d137baa5d055
mjstring <mk.string@pm.me>
2021-09-17 18:00:11 -0500

    memoized selectCollection and collectionUrlParam, and completed routing to collection specific shopping pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import {connect} from 'react-redux';
-import CollectionItem from '../../components/collection-item/collection-item.component';
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-import './collection.styles.scss';
+import "./collection.styles.scss";
 
-const CollectionPage = ({collection}) => 
-{    console.log(collection);
-    return (<div className='collection-page'>
-        <h2>CATEGORY PAGE</h2>
-    </div>)}
+const CollectionPage = ({ collections }) => {
+  const { title, items } = collections;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-const mapStateToProps = (state, ownProps) =>({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
+const mapStateToProps = (state, ownProps) => ({
+  collections: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
-import CollectionPage from '../collection/collection.component'
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({match}) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <Route exact path ={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
