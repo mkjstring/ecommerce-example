1193add526e40f34d0f8301df6c96234acd4a888
mjstring <mk.string@pm.me>
2021-09-20 18:24:28 -0500

    added stripe test integration and browser tab title and icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JbqU7IYNdm1XacEW5W3SYQyPRc5mh4LvzbOfOV5wZUB6JhZTCT4V7WtQJKBlpYWhAbIazCZyXIuXp25ZSpRwiVR006erzq8SN";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Ecomm Template"
+      billingAddress
+      shippingAddress
+      image=""
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
